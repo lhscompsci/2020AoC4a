06306347772cfc07772b410db806724f75e53655
lhscompsci <sam.black@lubbockisd.org>
2020-12-04 00:48:46 -0600

    Not quite there yet still

--- src/com/wtxcompsci/Main.java
+++ src/com/wtxcompsci/Main.java
@@ -3,24 +3,56 @@ package com.wtxcompsci;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner inf = new Scanner(new File(("input.dat")));
 
-        long ans = 1L;
 
-        ArrayList<String> map = new ArrayList<>();
+        ArrayList<Map<String, String>> pptList = new ArrayList<>();
 
-        while(inf.hasNext()){
-            map.add(inf.nextLine());
+        Map<String, String> m = new TreeMap<>();
+
+        int counter = 0;
+
+        while (inf.hasNextLine()) {
+            String line = inf.nextLine();
+            if (!line.equals("")) {
+                Scanner parts = new Scanner(line);
+                while (parts.hasNext()) {
+                    Scanner part = new Scanner(parts.next());
+                    part.useDelimiter(":");
+                    m.put(part.next(), part.next());
+                }
+            } else {
+                System.out.println("set "+ ++counter+": "+m);
+                pptList.add(m);
+                m = new TreeMap<>();
+            }
         }
 
-        
-        
-        
+        System.out.println();
+        System.out.println(pptList);
+        System.out.println();
+
+        int ans = 0;
+
+        for (Map<String, String> stringStringMap : pptList) {
+            System.out.println(stringStringMap);
+            if (stringStringMap.keySet().size() == 8)
+                ans++;
+            if ((stringStringMap.keySet().size() == 7) && !stringStringMap.containsKey("cid"))
+                ans++;
+
+        }
+
+        //203 is too low
+        //259 is too high
+
         System.out.println(ans);
     }
 }
